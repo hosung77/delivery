ffbeca3a1fdfe1ee4b6c1409a4587c4558bc6101
aksirjwn <skks1000@naver.com>
2025-04-27 23:47:14 +0900

    feat, fix : 로그아웃, 메뉴 버그 수정 및 테스트

--- src/main/java/com/example/delivery/config/filter/RoleFilter.java
+++ src/main/java/com/example/delivery/config/filter/RoleFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
