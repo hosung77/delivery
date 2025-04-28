f8b7c4f46562594b7da88f3358423eb084cff44f
hosung77 <ho4728@gmail.com>
2025-04-28 20:12:35 +0900

    fix : api 수정

--- src/main/java/com/example/delivery/controller/order/OrderController.java
+++ src/main/java/com/example/delivery/controller/order/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/order")
+@RequestMapping("/api/stores/order")
 @RequiredArgsConstructor
 public class OrderController {
 
